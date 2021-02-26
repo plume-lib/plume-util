88c3afd094e28eb916ca3e33dff862f48d708b70
Michael Ernst <mernst@cs.washington.edu>
2021-02-25 18:27:11 -0800

    Add file FilesPlumeTest.java

---
+++ src/test/java/org/plumelib/FilesPlumeTest.java
@@ -0,0 +1,57 @@
+package org.plumelib.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({
+  "UseCorrectAssertInTests" // `assert` works fine in tests
+})
+public final class UtilPlumeTest {
+
+  // public static BufferedReader bufferedFileReader(String filename)
+  // public static LineNumberReader lineNumberFileReader(String filename)
+  // public static BufferedWriter bufferedFileWriter(String filename) throws IOException
+  // public static Class classForName(String className)
+
+  // public static void addToClasspath(String dir)
+  // public static final class WildcardFilter implements FilenameFilter
+  //   public WildcardFilter(String filename)
+  //   public boolean accept(File dir, String name)
+  // public static boolean canCreateAndWrite(File file)
+  // public static void writeObject(Object o, File file) throws IOException
+  // public static Object readObject(File file)
+  // public static File createTempDir(String prefix, String suffix)
+
+  // public Object incrementHashMap(HashMap hm, Object key, int count)
+
+  @Test
+  public void test_canCreateAndWrite() {
+
+    try {
+      assertTrue(FilesPlume.canCreateAndWrite(new File("TestPlume.java")));
+
+      // This test fails if run by the superuser (who can overwrite
+      // any file).
+      if (!System.getProperty("user.name").equals("root")) {
+        File readOnly = new File("temp");
+        readOnly.createNewFile();
+        readOnly.setReadOnly();
+        assertTrue(!FilesPlume.canCreateAndWrite(readOnly));
+        readOnly.delete();
+      }
+
+      assertTrue(FilesPlume.canCreateAndWrite(new File("temp")));
+      assertTrue(!FilesPlume.canCreateAndWrite(new File("temp/temp")));
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail("failure while testing FilesPlume.canCreateAndWrite(): " + e.toString());
+    }
+  }
+
+  // public static void streamCopy(java.io.InputStream from, java.io.OutputStream to)
+
+}
