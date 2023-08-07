0814feb2ec83178fdcd1fec253edf6df267c99c8
Suzanne Millstein <smillst@cs.washington.edu>
2023-08-07 11:38:51 -0700

    Suppression not needed.

--- src/main/java/org/plumelib/util/ArraySet.java
+++ src/main/java/org/plumelib/util/ArraySet.java
@@ -55,8 +55,7 @@ import org.checkerframework.dataflow.qual.SideEffectFree;
   "index", // TODO
   "keyfor", // https://tinyurl.com/cfissue/4558
   "lock", // not yet annotated for the Lock Checker
-  "nullness", // temporary; nullness is tricky because of null-padded arrays
-  "mustcall"
+  "nullness" // temporary; nullness is tricky because of null-padded arrays
 })
 public class ArraySet<E extends @UnknownSignedness Object> extends AbstractSet<E> {
 
