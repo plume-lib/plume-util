30146f598ebf04d06ab4903cb10b34ae39661e37
Damika-Anupama <damikaanupama@gmail.com>
2025-09-03 22:16:04 -0500

    Removed unnecessary bounds check

--- src/main/java/org/plumelib/util/RandomSelector.java
+++ src/main/java/org/plumelib/util/RandomSelector.java
@@ -146,9 +146,7 @@ public class RandomSelector<T> {
         @SuppressWarnings("lowerbound:argument") // no list support
         int rem = generator.nextInt(values.size());
         // values should be MinLen(1), meaning that values.size() is positive.
-        if (rem < values.size()) {
-          values.set(rem, next);
-        }
+        values.set(rem, next);
       }
     }
     // do nothing if the probability condition is not met
