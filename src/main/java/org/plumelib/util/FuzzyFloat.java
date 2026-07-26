2e302d188e27361647c3d45a2121367f7cae6d7c
Michael Ernst <mernst@cs.washington.edu>
2026-07-26 10:31:30 -0700

    Improvements to FuzzyFloat and MathP

--- src/main/java/org/plumelib/util/FuzzyFloat.java
+++ src/main/java/org/plumelib/util/FuzzyFloat.java
@@ -14,11 +14,19 @@ import org.checkerframework.dataflow.qual.Pure;
  * <p>Floating-point numbers are compared for equality by dividing them by one another and comparing
  * the ratio. By default, they must be within 0.0001 (0.01%) to be considered equal.
  *
- * <p>Zero is never considered equal to a non-zero number, no matter how small its value.
+ * <p>Zero is considered equal to a non-zero number only if the non-zero number's magnitude is less
+ * than the square of the fuzzy ratio.
  *
  * <p>Two NaN floats are not considered equal (consistent with the == operator).
+ *
+ * <p>This class is serializable so that its non-static inner class {@link
+ * DoubleArrayComparatorLexical}, which holds an implicit reference to the enclosing FuzzyFloat, can
+ * be serialized.
  */
-public class FuzzyFloat {
+public class FuzzyFloat implements Serializable {
+
+  /** Unique identifier for serialization. If you add or remove fields, change this number. */
+  private static final long serialVersionUID = 20250723L;
 
   /** Default relative difference between two values such that this class considers them equal. */
   static final double DEFAULT_RELATIVE_RATIO = .0001;
